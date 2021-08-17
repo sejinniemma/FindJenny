a3937a28071655f8388b6dea4c69b6824b606cb5
Emma <jsj0471@naver.com>
2021-08-17 21:50:11 +0900

    make timer

--- main.js
+++ main.js
@@ -7,14 +7,18 @@ const gameContainer = document.querySelector('.game__container');
 const startBanner = document.querySelector('.game__start__banner');
 const gameTextMessage = document.querySelector('.game__banner');
 const startBtn = document.querySelector('.startBtn');
+const gameTimer = document.querySelector('.game__timer');
 
 let started = false;
+const timeDuration = 60;
+let timer;
 
 startBtn.addEventListener('click', initGame);
 function initGame() {
     startBanner.style.display = 'none';
     started = true;
     setTimeout(startGame,1000);
+    startTimer();
     }
     
 //클릭할때는 작동이 안되게 하고 클릭하고나서는 onClickField가 작동이 될 수 있도록
@@ -35,12 +39,33 @@ function onClickField(){
                 console.log('hi');
              }
          }
-        showTextMessage();
+         gameTextMessage.style.display = 'block';
     })
 }
   
 
 function showTextMessage(){
     gameTextMessage.style.display = 'block';
-    setTimeout(gameTextMessage.style.display = 'none',1000);
+    //시간멈추기
+    //게임스탑 started = false로 바꿈
+    //누르면 사라짐 or  1초있다 사라짐
+    // setTimeout(gameTextMessage.style.display = 'none',1000);
+}
+
+function startTimer(){
+    let setTimeDuration = timeDuration;
+    gameTimer.innerText = setTimeDuration;
+   timer = setInterval(() => {
+        showTimer(--setTimeDuration);
+         if(timeDuration === 0){
+             clearInterval(timer);
+         }
+    }, 1000);
+}
+
+function showTimer(time){
+    const minute = Math.floor(time / 60);
+    const second = time % 60;
+
+    gameTimer.innerText = `${minute} : ${second}`;
 }
