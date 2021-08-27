f8b4789728e8a0c6d8327e896a4fa2e27f4b2962
Emma <jsj0471@naver.com>
2021-08-27 22:49:48 +0900

    finalWInMessage

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const gameContainer = document.querySelector('.game__container');
 const startBanner = document.querySelector('.game__start__banner');
 const TryAgainMessage = document.querySelector('.game__banner1');
 const LevelUpMessage = document.querySelector('.game__banner2');
+const FinalWinMessage = document.querySelector('.game__banner3');
 const startBtn = document.querySelector('.startBtn');
 const gameTimer = document.querySelector('.game__timer');
 const gameBackGround = document.querySelector('.game__playground');
@@ -58,10 +59,19 @@ function level2(verti,horoz) {
 
 function level3(verti,horoz) {
     if(verti > 716 && verti < 737 && horoz > 416 && horoz < 453){
-      gameWIn();
+        FinalWin();
    }else {
       gameLoose();
    }
+}
+
+function FinalWin(){
+    showTextMessage(FinalWinMessage);
+    hideTextMessage(TryAgainMessage);
+    clearInterval(timer);
+    musicPause(bgMusic);
+    musicPause(looseSound) 
+    musicPlay(winSound);
 }
 
 function gameWIn() {
@@ -117,16 +127,13 @@ LevelUpMessage.addEventListener('click', () => {
     onClickField(levelUp[i].lev);
 })
 
+
 function nextLevel (){
     gameBackGround.style.background = levelUp[i].src;
     hideTextMessage(LevelUpMessage);
     startTimer();
     musicPlay(bgMusic);
 }
-
-// 이겼을때 트라이어게인 메세지 딸려나오지 않게만들기
-// 마지막 이겼을 때 메세지와 게임종료
-// 더 효율적인 코드
 
 
 const levelUp = [
