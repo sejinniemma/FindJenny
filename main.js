893cc8d00830fdea7ae50504f66a28ff67b38bac
Emma <jsj0471@naver.com>
2021-10-07 19:27:21 +0900

    prettier🥰

--- main.js
+++ main.js
@@ -1,191 +1,171 @@
-'use strict';
+'use strict'
 
+const gameContainer = document.querySelector('.game__container')
+const startBanner = document.querySelector('.game__start__banner')
+const TryAgainMessage = document.querySelector('.game__banner1')
+const LevelUpMessage = document.querySelector('.game__banner2')
+const FinalWinMessage = document.querySelector('.game__banner3')
+const ReplayMessage = document.querySelector('.game__banner4')
+const startBtn = document.querySelector('.startBtn')
+const gameTimer = document.querySelector('.game__timer')
+const gameBackGround = document.querySelector('.game__playground')
 
-const gameContainer = document.querySelector('.game__container');
-const startBanner = document.querySelector('.game__start__banner');
-const TryAgainMessage = document.querySelector('.game__banner1');
-const LevelUpMessage = document.querySelector('.game__banner2');
-const FinalWinMessage = document.querySelector('.game__banner3');
-const ReplayMessage = document.querySelector('.game__banner4');
-const startBtn = document.querySelector('.startBtn');
-const gameTimer = document.querySelector('.game__timer');
-const gameBackGround = document.querySelector('.game__playground');
-
-
-let started = false;
-const timeDuration = 5;
-let timer;
-let i = 0;
+let started = false
+const timeDuration = 10
+let timer
+let i = 0
 
 // Game start
-startBtn.addEventListener('click', initGame);
+startBtn.addEventListener('click', initGame)
 
 function initGame() {
-    started = true;
-    startBanner.style.display = 'none';
-    setTimeout(startGame,1000);    
-    startTimer();
-    musicPlay(bgMusic);
-    }
+  started = true
+  startBanner.style.display = 'none'
+  setTimeout(startGame, 1000)
+  startTimer()
+  musicPlay(bgMusic)
+}
 
-function startGame(){
-        started === true && onClickField(levelUp[i].lev);
+function startGame() {
+  started === true && onClickField(levelUp[i].lev)
 }
 
 // onClickFiled & gameWinAndLoose
-function onClickField(stage){
-    gameContainer.addEventListener('click',(event)=>{
-        // if(!started){
-        //     return;
-        // }
-        //두번째 배열로 안감 근대 저걸해야 게임도중에 안눌리는데...왜 안가는지 알아내야함!!
-        
-        const x = event.clientX;
-        const y = event.clientY;
-    
-        stage(x,y);
-        musicPlay(looseSound);
+function onClickField(stage) {
+  gameContainer.addEventListener('click', (event) => {
+    const x = event.clientX
+    const y = event.clientY
 
-//왜???? 왜 여기서 그냥 막혀버려 ??
-    })
+    stage(x, y)
+    musicPlay(looseSound)
+  })
 }
 
-function level1(verti,horoz) {
-    if(verti > 980 && verti < 995 && horoz > 622 && horoz < 662){
-      gameWIn();
-   }else {
-      gameLoose();
-   }
+function level1(verti, horoz) {
+  if (verti > 980 && verti < 995 && horoz > 622 && horoz < 662) {
+    gameWIn()
+  } else {
+    gameLoose()
+  }
 }
 
-function level2(verti,horoz) {
-    if(verti > 780 && verti < 797 && horoz > 172 && horoz < 203){
-      gameWIn();
-   }else {
-      gameLoose();
-   }
+function level2(verti, horoz) {
+  if (verti > 780 && verti < 797 && horoz > 172 && horoz < 203) {
+    gameWIn()
+  } else {
+    gameLoose()
+  }
 }
 
-function level3(verti,horoz) {
-    if(verti > 716 && verti < 737 && horoz > 416 && horoz < 453){
-        FinalWin();
-   }else {
-      gameLoose();
-   }
+function level3(verti, horoz) {
+  if (verti > 716 && verti < 737 && horoz > 416 && horoz < 453) {
+    FinalWin()
+  } else {
+    gameLoose()
+  }
 }
 
-function FinalWin(){
-    finishGame(FinalWinMessage,finalWinSound);
+function FinalWin() {
+  finishGame(FinalWinMessage, finalWinSound)
 }
 
 function gameWIn() {
-    finishGame(LevelUpMessage,winSound);
+  finishGame(LevelUpMessage, winSound)
 }
 
-function finishGame(text,sound){
-    started = false;
-    showTextMessage(text);
-    hideTextMessage(TryAgainMessage);
-    clearInterval(timer);
-    musicPause(bgMusic);
-    musicPlay(sound);
+function finishGame(text, sound) {
+  started = false
+  showTextMessage(text)
+  hideTextMessage(TryAgainMessage)
+  clearInterval(timer)
+  musicPause(bgMusic)
+  musicPlay(sound)
 }
 
 function gameLoose() {
-    started = false;
-    showTextMessage(TryAgainMessage);
-    
-    TryAgainMessage.addEventListener('click',()=>{
-        started = true;
-       TryAgainMessage.style.display = 'none'; 
-    })   
-} 
+  showTextMessage(TryAgainMessage)
 
-// Show Text Message
-function showTextMessage(message){
-    message.style.display = 'block';
+  TryAgainMessage.addEventListener('click', () => {
+    TryAgainMessage.style.display = 'none'
+  })
 }
 
-function hideTextMessage(message){
-    message.style.display = 'none';
+// Show Text Message
+function showTextMessage(message) {
+  message.style.display = 'block'
+}
+
+function hideTextMessage(message) {
+  message.style.display = 'none'
 }
 
 // Timer
-function startTimer(){
-    let setTimeDuration = timeDuration;
-    showTimer(setTimeDuration);
-   timer = setInterval(() => {
-        showTimer(--setTimeDuration);
-         if(setTimeDuration === 0){
-             clearInterval(timer);
-             musicPause(bgMusic);
-             showTextMessage(ReplayMessage);
-             hideTextMessage(TryAgainMessage);
-             started = false;
-         }
-    }, 1000);
+function startTimer() {
+  let setTimeDuration = timeDuration
+  showTimer(setTimeDuration)
+  timer = setInterval(() => {
+    showTimer(--setTimeDuration)
+    if (setTimeDuration === 0) {
+      clearInterval(timer)
+      musicPause(bgMusic)
+      showTextMessage(ReplayMessage)
+      hideTextMessage(TryAgainMessage)
+    }
+  }, 1000)
 }
 
-function showTimer(time){
-    const minute = Math.floor(time / 60);
-    const second = time % 60;
+function showTimer(time) {
+  const minute = Math.floor(time / 60)
+  const second = time % 60
 
-    gameTimer.innerText = `${minute} : ${second}`;
+  gameTimer.innerText = `${minute} : ${second}`
 }
-
 
 LevelUpMessage.addEventListener('click', () => {
-    started = true;
-    i++;
-    nextLevel();
-    startGame();
+  started = true
+  i++
+  nextLevel()
+  startGame()
 })
 
-ReplayMessage.addEventListener('click',()=>{
-    started = true;
-    startTimer();
-    musicPlay(bgMusic);
-    startGame();
-    hideTextMessage(ReplayMessage);
-});
+ReplayMessage.addEventListener('click', () => {
+  startTimer()
+  musicPlay(bgMusic)
+  startGame()
+  hideTextMessage(ReplayMessage)
+})
 
-
-function nextLevel (){
-    gameBackGround.style.background = levelUp[i].src;
-    hideTextMessage(LevelUpMessage);
-    startTimer();
-    musicPlay(bgMusic);
+function nextLevel() {
+  gameBackGround.style.background = levelUp[i].src
+  hideTextMessage(LevelUpMessage)
+  startTimer()
+  musicPlay(bgMusic)
 }
-
 
 const levelUp = [
-    {
-        lev : level1,
-        src : 'url(imgs/jenny/level1.jpeg) no-repeat center/cover'
-    },
-    {
-        lev : level2,
-        src : 'url(imgs/jenny/level2.jpeg) no-repeat center/cover'
-    },
-    {
-        lev : level3,
-        src : 'url(imgs/jenny/level3.jpeg) no-repeat center/cover'
-    }
+  {
+    lev: level1,
+    src: 'url(imgs/jenny/level1.jpeg) no-repeat center/cover',
+  },
+  {
+    lev: level2,
+    src: 'url(imgs/jenny/level2.jpeg) no-repeat center/cover',
+  },
+  {
+    lev: level3,
+    src: 'url(imgs/jenny/level3.jpeg) no-repeat center/cover',
+  },
 ]
 
-
-
 // Music
-const winSound = new Audio('music/뚝배기.mp3');
-const bgMusic = new Audio('music/clock.wav');
-const looseSound = new Audio('music/바운스.mp3');
-const finalWinSound = new Audio('music/final.wav');
-function musicPlay(sound){
-    sound.play();
+const winSound = new Audio('music/뚝배기.mp3')
+const bgMusic = new Audio('music/clock.wav')
+const looseSound = new Audio('music/바운스.mp3')
+const finalWinSound = new Audio('music/final.wav')
+function musicPlay(sound) {
+  sound.play()
 }
 
-function musicPause(sound){
-    sound.pause();
+function musicPause(sound) {
+  sound.pause()
 }
-
-//
